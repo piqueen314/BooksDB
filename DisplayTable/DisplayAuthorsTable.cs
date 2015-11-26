ac3e612e9565e55c0e4404796993d5f4cf736da4
Ceceilia Hedrick <cecehedrick@gmail.com>
2015-11-25 18:35:17 -0600

    first commit

---
+++ DisplayTable/DisplayAuthorsTable.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+
+namespace DisplayTable
+{
+    public partial class DisplayAuthorsTable : Form
+    {
+        public DisplayAuthorsTable()
+        {
+            InitializeComponent();
+        }
+
+        //Entity Framework DbContext
+        private BooksExamples.BooksEntities dbcontext = new BooksExamples.BooksEntities();
+
+        //Load data from db into data grid view
+
+
+        private void authorBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            Validate();
+            authorBindingSource.EndEdit();
+            try
+            {
+                dbcontext.SaveChanges();
+            }
+            catch (DbEntityValidationException)
+            {
+                MessageBox.Show("FirstName and LastName must contain values", "Entity Validation Exception");
+            }
+        }
+
+        //Load data from db into data grid view
+        private void DisplayAuthorsTable_Load(object sender, EventArgs e)
+        {
+
+            dbcontext.Authors
+                .OrderBy(author => author.LastName)
+                .ThenBy(author => author.FirstName)
+                .Load();
+            //specifies the data source for authorBindingSource that was created  in design
+            authorBindingSource.DataSource = dbcontext.Authors.Local;
+           
+
+        }
+        
+
+
+    }
+}
